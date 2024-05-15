f66af028e64353c64dafa9f447f3b9964a323ad4
HoYoung1 <kimo722504@gmail.com>
2024-05-15 18:46:10 +0800

    What I Learned Today - Local Inner Class

---
+++ java-mid1/src/nested/nested/local/LocalOuterV2.java
@@ -0,0 +1,33 @@
+package nested.nested.local;
+
+public class LocalOuterV2 {
+    private int outInstanceVar = 3;
+
+    public void process(int paramVar) {
+        int localVar = 1;
+
+        class LocalPrint implements Printer {
+            int value = 0;
+
+            public void printData() {
+                System.out.println("value = " + value);
+                System.out.println("localVar = " + localVar);
+                System.out.println("paramVar = " + paramVar);
+                System.out.println("outInstanceVar = " + outInstanceVar);
+            }
+
+            @Override
+            public void print() {
+                printData();
+            }
+        }
+
+        LocalPrint printer = new LocalPrint();
+        printer.printData();
+    }
+
+    public static void main(String[] args) {
+        LocalOuterV2 localOuterV1 = new LocalOuterV2();
+        localOuterV1.process(2);
+    }
+}
---
+++ java-mid1/src/nested/nested/local/LocalOuterV4.java
@@ -0,0 +1,45 @@
+package nested.nested.local;
+
+import java.lang.reflect.Field;
+
+public class LocalOuterV4 {
+    private int outInstanceVar = 3;
+
+    public Printer process(int paramVar) {
+        final int localVar = 1; // 지역 변수는 스택 프레임이 종료되는 순간 함께 제거된다.
+
+        class LocalPrint implements Printer {
+            int value = 0;
+
+            public void printData() {
+                System.out.println("value = " + value);
+                System.out.println("localVar = " + localVar);
+                System.out.println("paramVar = " + paramVar);
+                System.out.println("outInstanceVar = " + outInstanceVar);
+            }
+
+            // 인스턴스는 지역 변수보다 더 오래 살아남는다.
+            @Override
+            public void print() {
+                printData();
+            }
+
+        }
+
+        LocalPrint printer = new LocalPrint();
+//        printer.printData(); // 여기서 실행하지 않고
+//        localVar = 10;
+        return printer;
+    }
+
+    public static void main(String[] args) {
+        LocalOuterV4 localOuterV1 = new LocalOuterV4();
+        Printer printer = localOuterV1.process(2);
+        printer.print();
+
+        Field[] fields = printer.getClass().getFields();
+        for (Field field : fields) {
+            System.out.println(field);
+        }
+    }
+}
